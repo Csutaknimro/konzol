cde1353378e2372f2f97063f1a95cc8d13c31da5
Csutaknimro <nimrodcsutak@gmail.com>
2026-03-19 14:42:18 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
 radio.setGroup(1)
-radio.sendNumber(0)
-radio.raiseEvent(
-EventBusSource.MICROBIT_ID_BUTTON_A,
-EventBusValue.MES_REMOTE_CONTROL_EVT_PLAY
-)
 basic.forever(function () {
 	
 })
