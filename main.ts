5666964fd311dce4989bf578854aff7271c54c65
Csutaknimro <nimrodcsutak@gmail.com>
2026-03-26 14:15:24 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-	
+    radio.raiseEvent(
+    JoyBit.rockerY(),
+    EventBusValue.MES_REMOTE_CONTROL_EVT_PLAY
+    )
 })
 radio.setGroup(1)
 basic.forever(function () {
